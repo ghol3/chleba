1f4f5c3adac74865af715f300937765d186f7b58
ghol3 <ghol3@seznam.cz>
2013-03-28 19:52:20 +0100

    ahoj chlebaku... xD

---
+++ Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace chleby
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.BGforMOVE = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BGforMOVE)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Yellow;
+            this.pictureBox1.Location = new System.Drawing.Point(15, 157);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(118, 80);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(12, 240);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(53, 240);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // BGforMOVE
+            // 
+            this.BGforMOVE.BackgroundImage = global::chleby.Properties.Resources.notes_lista;
+            this.BGforMOVE.Location = new System.Drawing.Point(231, 0);
+            this.BGforMOVE.Name = "BGforMOVE";
+            this.BGforMOVE.Size = new System.Drawing.Size(210, 25);
+            this.BGforMOVE.TabIndex = 3;
+            this.BGforMOVE.TabStop = false;
+            this.BGforMOVE.MouseDown += new System.Windows.Forms.MouseEventHandler(this.BGforMOVE_MouseDown);
+            this.BGforMOVE.MouseMove += new System.Windows.Forms.MouseEventHandler(this.BGforMOVE_MouseMove);
+            this.BGforMOVE.MouseUp += new System.Windows.Forms.MouseEventHandler(this.BGforMOVE_MouseUp);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Fuchsia;
+            this.pictureBox2.Location = new System.Drawing.Point(-7, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(238, 25);
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Location = new System.Drawing.Point(240, 55);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(143, 133);
+            this.textBox1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(441, 279);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.BGforMOVE);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.TransparencyKey = System.Drawing.Color.Fuchsia;
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.Move += new System.EventHandler(this.Form1_Move);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BGforMOVE)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox BGforMOVE;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace chleby
+{
+    public partial class Form1 : Form
+    {
+        bool pohyb = false;
+        private int a = 50;
+        private int b = 50;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            this.Location = new Point(a, b);
+            if (pohyb)
+                pictureBox1.Location = new Point(MousePosition.X - a, MousePosition.Y - b);
+        }
+
+        private void Form1_Move(object sender, EventArgs e)
+        {
+            a = this.Location.X;
+            b = this.Location.Y;
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            pohyb = true;
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            pohyb = false;
+            //testovani rohu
+            if (pictureBox1.Location.X < 2 && pictureBox1.Location.Y > 277 - pictureBox1.Height)
+                pictureBox1.Location = new Point(2, 277 - pictureBox1.Height);
+            if(pictureBox1.Location.X < 2 && pictureBox1.Location.Y < 27)
+                pictureBox1.Location = new Point(2, 27);
+            if(pictureBox1.Location.X > 439 - pictureBox1.Width && pictureBox1.Location.Y < 27)
+                pictureBox1.Location = new Point(439 - pictureBox1.Width, 27);
+            if(pictureBox1.Location.X > 439 - pictureBox1.Width && pictureBox1.Location.Y > 277 - pictureBox1.Height)
+                pictureBox1.Location = new Point(439 - pictureBox1.Width, 277 - pictureBox1.Height);
+            //testovani jen stran
+            if (pictureBox1.Location.X < 2)
+                pictureBox1.Location = new Point(2, MousePosition.Y - b);
+            if (pictureBox1.Location.X > 439 - pictureBox1.Width)
+                pictureBox1.Location = new Point(439 - pictureBox1.Width, MousePosition.Y - b);
+            if (pictureBox1.Location.Y > 277 - pictureBox1.Height)
+                pictureBox1.Location = new Point(MousePosition.X - a, 277 - pictureBox1.Height);
+            if (pictureBox1.Location.Y < 27)
+                pictureBox1.Location = new Point(MousePosition.X - a, 27);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Text = MousePosition.X.ToString();
+            label2.Text = MousePosition.Y.ToString();
+            
+        }
+
+        //pohybovani s oknem
+        int WindowMove, WindowMoveX, WindowMoveY;
+        private void BGforMOVE_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (WindowMove == 1)
+                this.SetDesktopLocation(MousePosition.X - WindowMoveX - 230, MousePosition.Y - WindowMoveY);
+        }
+
+        private void BGforMOVE_MouseDown(object sender, MouseEventArgs e)
+        {
+            WindowMove = 1;
+            WindowMoveX = e.X;
+            WindowMoveY = e.Y;
+        }
+
+        private void BGforMOVE_MouseUp(object sender, MouseEventArgs e)
+        {
+            WindowMove = 0;
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {   
+            Brush brush = new SolidBrush(Color.FromArgb(49, 48, 48));
+            Rectangle horni = new Rectangle(0, 25, 441, 2);
+            Rectangle leva = new Rectangle(0,25, 2, 279);
+            Rectangle spodni = new Rectangle(0, 277, 441, 2);
+            Rectangle prava = new Rectangle(439, 25, 2, 279);
+            e.Graphics.FillRectangle(brush, horni);
+            e.Graphics.FillRectangle(brush, leva);
+            e.Graphics.FillRectangle(brush, spodni);
+            e.Graphics.FillRectangle(brush, prava);
+        }
+    }
+}
